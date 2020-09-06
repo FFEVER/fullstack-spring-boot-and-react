0efe362f895ace59f70ccce651e65cbed7d7c168
Nattaphol <rootncdev2015@gmail.com>
2020-09-06 23:52:56 +0700

    JPA: Add query method for CarRepository

--- cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,6 +1,36 @@
 package com.packt.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    // Fetch cars by brand
+    List<Car> findByBrand(String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(String color);
+
+    // Fetch cars by year
+    List<Car> findByYear(Integer year);
+
+    // Fetch cars by brand and model
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    // Fetch cars by brand or color
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    // Fetch cars by brand and sort by year
+    List<Car> findByBrandOrderByYearAsc(String brand);
+
+    // Fetch cars by registerNumber using SQL
+    @Query("select c from Car c where c.registerNumber = ?1")
+    List<Car> findByRegisterNumber(String registerNumber);
+
+    // Fetch cars by brand using SQL
+    @Query("select c from Car c where c.brand like %?1%")
+    List<Car> findByBrandContains(String brand);
+
 }
