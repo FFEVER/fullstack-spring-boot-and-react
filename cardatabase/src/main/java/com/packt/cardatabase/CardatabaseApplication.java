7e3670db34d0fe3c938ae68cec49694629fa2579
Nattaphol <rootncdev2015@gmail.com>
2020-09-07 00:55:11 +0700

    JPA: Add example of relationship

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,6 +2,8 @@ package com.packt.cardatabase;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,8 @@ public class CardatabaseApplication {
 
     @Autowired
     private CarRepository carRepository;
+    @Autowired
+    private OwnerRepository ownerRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
@@ -27,11 +31,17 @@ public class CardatabaseApplication {
     @Bean
     CommandLineRunner runner() {
         return args -> {
-            carRepository.save(new Car("Tesla", "Model S", "Space Grey", "DGB-3333", 2020, 30000));
-            carRepository.save(new Car("Tesla", "Model X", "Black", "DGS-5555", 2018, 63000));
-            carRepository.save(new Car("Tesla", "Model X", "White", "DGS-6666", 2017, 63000));
-            carRepository.save(new Car("Ford", "Mustang", "Red", "AAG-1234", 2014, 59000));
-            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+            Owner owner1 = new Owner("Nattaphol", "Johnas");
+            Owner owner2 = new Owner("Ornprapha", "Johnas");
+
+            ownerRepository.save(owner1);
+            ownerRepository.save(owner2);
+
+            carRepository.save(new Car("Tesla", "Model S", "Space Grey", "DGB-3333", 2020, 30000, owner1));
+            carRepository.save(new Car("Tesla", "Model X", "Black", "DGS-5555", 2018, 63000, owner1));
+            carRepository.save(new Car("Tesla", "Model X", "White", "DGS-6666", 2017, 63000, owner2));
+            carRepository.save(new Car("Ford", "Mustang", "Red", "AAG-1234", 2014, 59000, owner1));
+            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, owner2));
 
             logger.info("findByBrandContains(): {}", carRepository.findByBrandContains("yo"));
             logger.info("findByBrandOrderByYearAsc(): {}", carRepository.findByBrandOrderByYearAsc("Tesla"));
