9a1195e63a08ef155d93e6e66179944d81e9a90f
Nattaphol <rootncdev2015@gmail.com>
2020-09-06 23:42:31 +0700

    JPA: Test inserting cars into db

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,18 +1,35 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
+    @Autowired
+    private CarRepository carRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CardatabaseApplication.class, args);
         logger.info("Hello Spring Boot");
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            carRepository.save(new Car("Tesla", "Model S", "Space Grey", "DGB-3333", 2020, 50000));
+            carRepository.save(new Car("Ford", "Mustang", "Red", "AAG-1234", 2014, 59000));
+            carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000));
+        };
     }
 
 }
