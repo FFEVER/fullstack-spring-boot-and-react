ccbc691c2dfdc3bcd628547997c2e8ef82409ba8
Nattaphol <rootncdev2015@gmail.com>
2020-11-26 23:42:33 +0700

    Test: Implement test cases

--- cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,23 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.web.CarController;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class CardatabaseApplicationTests {
+	@Autowired
+	private CarController carController;
 
 	@Test
 	void contextLoads() {
+		assertThat(carController).isNotNull();
 	}
 
 }
